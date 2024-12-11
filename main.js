1bd5c8713dd8efcf9757a246424c0c3814ef7c0e
andrew-brazzino <andrew.gilgenberg@brazinomarketing.com>
2024-12-11 20:49:32 +0500

    Сделал запуск с nodemon, сконфигурировал nodemon. Сделал запуск и загрузку всех модулей ботом.

---
+++ main.js
@@ -0,0 +1,47 @@
+const { Telegraf, Scenes } = require('telegraf');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+const LocalSession = require('telegraf-session-local');
+
+const botToken = process.env.TELEGRAM_BOT_TOKEN;
+
+if (!botToken) {
+  process.exit(1);
+}
+
+const bot = new Telegraf(botToken);
+
+const { Stage } = Scenes;
+
+// Использование локальной сессии
+const localSession = new LocalSession({ database: 'session_db.json' });
+bot.use(localSession.middleware());
+
+// Подготовка Stage для сцен
+const stage = new Stage();
+bot.use(stage.middleware());
+
+
+// Регистрация сцен
+const scenesPath = path.join(__dirname, 'scenes');
+fs.readdirSync(scenesPath).forEach(file => {
+  if (file.endsWith('.js')) {
+    const scene = require(`./scenes/${file}`);
+    stage.register(scene);
+  }
+});
+
+// Регистрация команд
+const commandsFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
+commandsFiles.forEach(file => {
+  const command = require(`./commands/${file}`);
+  if (command.middleware) {
+    bot.command(command.command, command.middleware, command.action);
+  } else {
+    bot.command(command.command, command.action);
+  }
+});
+
+// Запуск бота
+bot.launch();
