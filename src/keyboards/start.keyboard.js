b72fe764a3405969508991fc00b1758e3093cbe1
andrew-brazzino <andrew.gilgenberg@brazinomarketing.com>
2024-12-13 21:03:44 +0500

    сделал клавиатуру с кнопкой назад

--- src/keyboards/start.keyboard.js
+++ src/keyboards/start.keyboard.js
@@ -1,8 +1,8 @@
 const { Markup } = require('telegraf');
 const ruMessage = require('../lang/ru.json');
 
-function start() {
-    return Markup.keyboard(ruMessage.keyboards.start).resize().oneTime();
+function back() {
+    return Markup.keyboard(ruMessage.keyboards.back).resize().oneTime();
 }
 
-module.exports = { start };
+module.exports = { back };
