ad9f8ffb7f2d83d4364ca5a585dd5e6eb32b82e8
andrew-brazzino <andrew.gilgenberg@brazinomarketing.com>
2024-12-18 13:19:46 +0500

    сделал обработку команды /admin

--- src/commands/admin.command.js
+++ src/commands/admin.command.js
@@ -1,15 +1,16 @@
 const ruMessage = require('../lang/ru.json');
 const { start } = require('../keyboards/start.keyboard');
+const { isAdmin } = require('../middlewares/isAdmin');
 
 module.exports = {
-    command: 'start',
-    description: 'Start command',
+    command: 'admin',
+    description: 'Admin start command',
     action: async (ctx) => {
-
         await ctx.telegram.sendMessage(
             ctx.from.id,
             ruMessage.messages.start.replace("{name}", ctx.from.first_name),
             start())
-        }
+        },
+        middleware: isAdmin
     }
 
